6e3b7b34f4a9841857ec45d79603f0c0d3a7d5a4
Yulia0808 <iul.sedova2014@yandex.ru>
2022-09-12 21:26:30 +0300

    lesson2

--- src/main/java/by/it_academy/lesson2/main.java
+++ src/main/java/by/it_academy/lesson2/main.java
@@ -1,7 +1,20 @@
 package by.it_academy.lesson2;
 
 public class main {
-    public static void main (String[] args) {
-        System.out.println("Hello, world!");
+    public static void main(String[] args) {
+        double first_num = 5.00;
+        double second_num = 7.00;
+
+        System.out.println("First value = " + first_num);
+        System.out.println("Second value = " + second_num);
+        System.out.println("Multiplication = " + first_num * second_num);
+        System.out.println("Division = " + first_num / second_num);
+
+
+        int a = ((2345 + 8) / 3) % -6;
+        System.out.println(a >= -10);
+        System.out.println(a < 9);
+        System.out.println(a == 0);
+        System.out.println(a != -3);
     }
 }
