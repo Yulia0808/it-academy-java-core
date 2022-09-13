bebf25bace2fec7da28e783ea6a09fd793878ee9
Yulia0808 <iul.sedova2014@yandex.ru>
2022-09-13 08:48:14 +0300

    lesson2 - second attempt

---
+++ src/main/java/by/it_academy/lesson2/Main.java
@@ -0,0 +1,23 @@
+package by.it_academy.lesson2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double firstNum = 5.00;
+        double secondNum = 7.00;
+
+        System.out.println("First value = " + firstNum);
+        System.out.println("Second value = " + secondNum);
+        System.out.println("Multiplication = " + firstNum * secondNum);
+        System.out.println("Division = " + firstNum / secondNum);
+
+
+        int a = ((2345 + 8) / 3) % -6;
+        System.out.println(a >= -10);
+        System.out.println(a < 9);
+        System.out.println(a == 0);
+        System.out.println(a != -3);
+    }
+}
+
+
