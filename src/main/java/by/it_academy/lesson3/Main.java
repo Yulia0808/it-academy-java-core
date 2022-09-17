0a9da64b1a545f6cff85431b5d9841628875fb7b
Yulia0808 <iul.sedova2014@yandex.ru>
2022-09-17 19:46:17 +0300

    lesson3 - attempt 2

--- src/main/java/by/it_academy/lesson3/Main.java
+++ src/main/java/by/it_academy/lesson3/Main.java
@@ -5,20 +5,18 @@ import org.w3c.dom.ls.LSOutput;
 public class Main {
     public static void main(String[] args) {
 
-        int giftValue = 500087;
+        int giftValue = 2510860;
+        double greatGift = 100 + (giftValue - 5000) * 0.08;
+        double veryGreatGift = 1700 + (giftValue - 25000) * 0.1;
 
         if (giftValue < 5000) {
             System.out.println("Tax amount = " + 0);
-        }
 
-        else if (giftValue >= 5000 && giftValue <= 25000) {
-            System.out.println("Tax amount = " + 100);
-        }
+        } else if (giftValue >= 5000 && giftValue <= 25000) {
+            System.out.println("Tax amount = " + greatGift);
 
-        else if (giftValue > 25000) {
-            double greatGiftValue = 100 + (giftValue - 5000) * 0.08;
-
-            System.out.println("Tax amount = " + greatGiftValue);
+        } else if (giftValue > 25000) {
+            System.out.println("Tax amount = " + veryGreatGift);
         }
     }
 }
