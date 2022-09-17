97f84e00f3adf58e3af48a750194dfe8c001ab54
Yulia0808 <iul.sedova2014@yandex.ru>
2022-09-17 21:02:26 +0300

    lesson3 - attempt 2, I understood everything)

--- src/main/java/by/it_academy/lesson3/Main.java
+++ src/main/java/by/it_academy/lesson3/Main.java
@@ -5,18 +5,18 @@ import org.w3c.dom.ls.LSOutput;
 public class Main {
     public static void main(String[] args) {
 
-        int giftValue = 25160;
-        double greatGift = 100 + (giftValue - 5000) * 0.08;
-        double veryGreatGift = 1700 + (giftValue - 25000) * 0.1;
+        int giftValue = 10000;
+        double tax = 0;
 
         if (giftValue < 5000) {
-            System.out.println("Tax amount = " + 0);
+            tax = 0;
 
         } else if (giftValue >= 5000 && giftValue <= 25000) {
-            System.out.println("Tax amount = " + greatGift);
+            tax = (100 + (giftValue - 5000) * 0.08);
 
         } else if (giftValue > 25000) {
-            System.out.println("Tax amount = " + veryGreatGift);
+            tax = (1700 + (giftValue - 25000) * 0.1);
         }
+        System.out.println("Tax anount is "+ tax);
     }
 }
