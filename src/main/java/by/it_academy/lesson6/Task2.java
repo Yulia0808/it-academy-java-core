a7cb1afd2a09df7ca693205890bada0af76e220d
Yulia0808 <iul.sedova2014@yandex.ru>
2022-09-29 21:27:41 +0300

    Lesson 5, вроде все исправила. Скобочки проверила - стоят на своих местах) Про вызов toString() помню, пока я соображаю плохо и медленно, поэтому оставляю себе подсказки.

---
+++ src/main/java/by/it_academy/lesson6/Task2.java
@@ -0,0 +1,21 @@
+package by.it_academy.lesson6;
+
+import java.util.regex.Pattern;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        String[] a = {"Sebastian,2015", "lucas,2017"};
+
+
+
+    }}
+       // StringBuilder len = new StringBuilder();
+      //  for (int i = 0;  i < a.length; i++) {
+      //  System.out.println(a[i]);}
+
+
+
+
+
+
